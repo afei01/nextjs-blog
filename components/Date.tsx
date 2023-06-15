789715fd1a3c867fb1ee874a73bfe27420e2edf3
fei <229317808@qq.com>
2023-06-15 16:12:24 +0800

    document: modify deployment.md

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from "date-fns";
 
 import React from "react";
+import { log } from "console";
 
 export default function Date({ dateString }: { dateString: string }) {
   const date = parseISO(dateString);
